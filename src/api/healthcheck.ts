cf76261310be9378c01f52506fbf4f4802acf0fe
aghosh0605 <aghosh0605@gmail.com>
2022-07-07 16:40:30 +0530

    ✨  API Healthcheck Improve

--- src/api/healthcheck.ts
+++ src/api/healthcheck.ts
@@ -1,17 +1,34 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from 'express';
 
 const healthCheckRoute = Router();
-healthCheckRoute.get("/", (req: Request, res: Response, next: NextFunction) => {
+const timeElapsed = Date.now();
+const today = new Date(timeElapsed);
+
+const formatTime = (seconds) => {
+  function pad(s) {
+    return (s < 10 ? '0' : '') + s;
+  }
+  let hours = Math.floor(seconds / (60 * 60));
+  let minutes = Math.floor((seconds % (60 * 60)) / 60);
+  let secs = Math.floor(seconds % 60);
+
+  return pad(hours) + ':' + pad(minutes) + ':' + pad(secs);
+};
+
+healthCheckRoute.get('/', (req: Request, res: Response, next: NextFunction) => {
   try {
     const healthcheckData = {
-      uptime: process.uptime(),
-      message: "⚙️ API v1 working!",
-      timestamp: Date.now(),
+      serverUptime: formatTime(process.uptime()),
+      osUptime: formatTime(require('os').uptime()),
+      message: '🛠️ API v1 working!',
+      timestamp: today.toUTCString(),
     };
     res.status(200).json(healthcheckData);
     next();
   } catch (e) {
-    res.status(503).send(e);
+    res
+      .status(503)
+      .send({ success: false, message: '🚫 API Health Check Failed' });
   }
 });
 
