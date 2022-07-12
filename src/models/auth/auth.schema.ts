9bd9ef6f326208060cf3687f6f4efd2cab558323
Pratham Bhatnagar <84272138+pratham-bhatnagar@users.noreply.github.com>
2022-07-13 00:22:32 +0530

    :sparkles: Add Auth schema

---
+++ src/models/auth/auth.schema.ts
@@ -0,0 +1,15 @@
+import * as yup from "yup";
+
+export const LoginRequestSchema = yup.object({
+  username: yup.string().required().trim(),
+  password: yup.string().required().trim(),
+});
+
+export type LoginRequest = yup.InferType<typeof LoginRequestSchema>;
+
+export const SignupRequestSchema = yup.object({
+  username: yup.string().trim().required(),
+  password: yup.string().trim().required(),
+});
+
+export type SignupRequest = yup.InferType<typeof SignupRequestSchema>;
