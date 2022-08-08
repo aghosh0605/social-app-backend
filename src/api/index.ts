d7ff437beff3d74e25d519cb052baf44b61f6098
Aniruddha Ghosh <aghosh0605@gmail.com>
2022-08-08 13:21:32 +0530

    🐛 JWT Validate Fucntion fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import chipsRoute from './chips/routes';
 import healthCheckRoute from './healthcheck';
 import authRoutes from './auth/routes';
-import { validateJWT } from '../middlewares/authentication/verify-jwt';
+import validateJWT from '../middlewares/authentication/verify-jwt';
 export default (): Router => {
   const app = Router();
 
   //TODO: add routes here...
   app.use('/auth', authRoutes);
   app.use('/', healthCheckRoute);
-  app.use('/posts', validateJWT(), chipsRoute);
+  app.use('/posts', validateJWT, chipsRoute);
 
   return app;
 };
--- src/middlewares/authentication/verify-jwt.ts
+++ src/middlewares/authentication/verify-jwt.ts
@@ -14,35 +14,35 @@ const JwtRequestSchema = yup.object({
 
 type JwtRequest = yup.InferType<typeof JwtRequestSchema>;
 
-export const validateJWT = () => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { authorization } = req.headers as JwtRequest;
-      if (!authorization) {
-        return next({
-          statusCode: 404,
-          message: 'No JWT Authorization Token available',
-        });
-      }
-      await JwtRequestSchema.validate(req.headers, { abortEarly: false });
-      const authToken = authorization.split(' ')[1];
-      verify(authToken, config.jwtSecret);
-      next();
-    } catch (err: Error | any) {
-      if (err.name === 'ValidationError') {
-        let message: string = '';
-        err.errors.forEach((error: string) => {
-          message += `${error}.\n `;
-        });
-        return next({
-          statusCode: 404,
-          message: message,
-        });
-      }
-      next({
-        statusCode: 500,
-        message: `${err.name}: ${err.message}`,
+const validateJWT = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { authorization } = req.headers as JwtRequest;
+    if (!authorization) {
+      return next({
+        statusCode: 404,
+        message: 'No JWT Authorization Token available',
       });
     }
-  };
+    await JwtRequestSchema.validate(req.headers, { abortEarly: false });
+    const authToken = authorization.split(' ')[1];
+    verify(authToken, config.jwtSecret);
+    next();
+  } catch (err: Error | any) {
+    if (err.name === 'ValidationError') {
+      let message: string = '';
+      err.errors.forEach((error: string) => {
+        message += `${error}.\n `;
+      });
+      return next({
+        statusCode: 404,
+        message: message,
+      });
+    }
+    next({
+      statusCode: 500,
+      message: `${err.name}: ${err.message}`,
+    });
+  }
 };
+
+export default validateJWT;
