98bcab553270dbcbd7c585463d9881bd0aa232b6
Aniruddha Ghosh <aghosh0605@gmail.com>
2022-08-11 15:26:41 +0530

    🔒️ Add express-rate-limiter with trust-proxy

--- src/api/healthcheck.ts
+++ src/api/healthcheck.ts
@@ -34,7 +34,7 @@ healthCheckRoute.get('/', (req: Request, res: Response, next: NextFunction) => {
       userInfo: os.userInfo(),
       serverUptime: formatTime(process.uptime()),
       osUptime: formatTime(os.uptime()),
-      reqIP: req.ip,
+      reqIP: req.ip, //reqIP==your public ip states that trust-proxy is correct in express server
     };
     res.status(200).json({ status: true, message: healthcheckData });
     next();
