5794596506de70231a4f9828c49c459c7b7846eb
Pratham Bhatnagar <prathambhatnagar2803@gmail.com>
2022-10-13 20:41:21 +0530

    Add Block User route

---
+++ src/api/users/controllers/block.service.ts
@@ -0,0 +1,37 @@
+import { Collection, ObjectId } from "mongodb";
+import { DBInstance } from "../../../loaders/database";
+import { NextFunction, Request, Response } from "express";
+import Logger from "../../../loaders/logger";
+
+export const blockUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req.params.id;
+    const usersCollection: Collection<any> = await (
+      await DBInstance.getInstance()
+    ).getCollection("users");
+    const user = req.user;
+
+    if (!(await usersCollection.findOne({ id }))) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found!",
+      });
+    } else {
+      await usersCollection.updateOne(
+        { _id: id },
+        { $push: { blockedUID: id } }
+      );
+      next();
+    }
+  } catch (err) {
+    Logger.error(err.errorStack || err);
+    res.status(err.statusCode || 500).json({
+      success: false,
+      message: err.message || "❌ Unknown Error Occurred!!",
+    });
+  }
+};
