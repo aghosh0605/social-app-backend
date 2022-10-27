882318fbaf7f5d35cde1e2d2330ba72f126893c6
Arvind-4 <lordarvindthegamer@gmail.com>
2022-10-27 21:55:09 +0530

    Updated: done with integration

--- src/api/posts/controllers/get.service.ts
+++ src/api/posts/controllers/get.service.ts
@@ -12,8 +12,37 @@ export const getAllPosts = async (
   try {
     const postsCollection: Collection<any> = await (
       await DBInstance.getInstance()
-    ).getCollection('posts');
-    const resData: postSchema[] = await postsCollection.find().toArray();
+    ).getCollection('posts'); 
+    const commentsCollection: Collection<any> = await (
+      await DBInstance.getInstance()
+    ).getCollection('comments');
+
+    const likesCollection: Collection<any> = await (
+      await DBInstance.getInstance()
+    ).getCollection('likes');
+
+    const posts = await postsCollection.find({}).toArray();
+    const comments = await commentsCollection.find({}).toArray();
+    const likes = await likesCollection.find({}).toArray();
+
+    let resData: postSchema[] = posts.map((post) => {
+      let postComments = comments.filter((comment) => {
+        return comment.postID === post._id.toString();
+      });
+      let postLikes = likes.filter((like) => {
+        return like.postID === post._id.toString();
+      });
+      let likeCount = postLikes.length;
+      let commentCount = postComments.length;
+      return { 
+        ...post, 
+        comments: postComments, 
+        likes: postLikes, 
+        likesCount: likeCount,
+        commentsCount: commentCount
+      };
+    });
+
     res.status(200).json({ 
       success: true, 
       message: "All Posts",
@@ -36,14 +65,39 @@ export const getUserPosts = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    //console.log(req.params.id);
     const postsCollection: Collection<any> = await (
       await DBInstance.getInstance()
-    ).getCollection('posts');
-    const resData: postSchema[] = await postsCollection
-      .find({ UID: req.params.id })
-      .toArray();
-    //console.log(resData);
+    ).getCollection('posts'); 
+    const commentsCollection: Collection<any> = await (
+      await DBInstance.getInstance()
+    ).getCollection('comments');
+
+    const likesCollection: Collection<any> = await (
+      await DBInstance.getInstance()
+    ).getCollection('likes');
+
+    const posts = await postsCollection.find({
+       UID: req.params.id 
+    }).toArray();
+    const comments = await commentsCollection.find({}).toArray();
+    const likes = await likesCollection.find({}).toArray();
+    let resData: postSchema[] = posts.map((post) => {
+      let postComments = comments.filter((comment) => {
+        return comment.postID === post._id.toString();
+      });
+      let postLikes = likes.filter((like) => {
+        return like.postID === post._id.toString();
+      });
+      let likeCount = postLikes.length;
+      let commentCount = postComments.length;
+      return { 
+        ...post, 
+        comments: postComments, 
+        likes: postLikes, 
+        likesCount: likeCount,
+        commentsCount: commentCount
+      };
+    });
     res.status(200).json({ 
       success: true, 
       message: "User Posts",
