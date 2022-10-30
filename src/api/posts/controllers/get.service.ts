47107cdd1955330b03df0587687f7e404c07b3cc
Arvind-4 <lordarvindthegamer@gmail.com>
2022-10-30 13:24:16 +0530

    Updated: Integrated Comments, Likes, and Circles

--- src/api/posts/controllers/get.service.ts
+++ src/api/posts/controllers/get.service.ts
@@ -13,48 +13,70 @@ export const getAllPosts = async (
     const postsCollection: Collection<any> = await (
       await DBInstance.getInstance()
     ).getCollection('posts'); 
-    const commentsCollection: Collection<any> = await (
-      await DBInstance.getInstance()
-    ).getCollection('comments');
 
-    const likesCollection: Collection<any> = await (
-      await DBInstance.getInstance()
-    ).getCollection('likes');
-
-    const circlesCollection = await (
-      await DBInstance.getInstance()
-    ).getCollection('circles');
-
-    const posts = await postsCollection.find({}).toArray();
-    const comments = await commentsCollection.find({}).toArray();
-    const likes = await likesCollection.find({}).toArray();
-    const circles = await circlesCollection.find({}).toArray();
-
-    let resData: postSchema[] = posts.map((post) => {
-      let postComments = comments.filter((comment) => {
-        return comment.postID === post._id.toString();
-      });
-      let postLikes = likes.filter((like) => {
-        return like.postID === post._id.toString();
-      });
-      let postCircles = circles.filter((circle) => {
-        return postsCollection.find({ 
-          circleID : new ObjectId(circle._id.toString())
-        }).toArray();
-      });
-      let likeCount = postLikes.length;
-      let commentCount = postComments.length;
-      console.log("postCircles", postCircles);
-      return { 
-        ...post, 
-        comments: postComments, 
-        likes: postLikes, 
-        likesCount: likeCount,
-        commentsCount: commentCount,
-        circles: postCircles
-      };
-    });
-
+    const resData = await postsCollection
+      .aggregate([
+        {
+          $lookup: {
+            from: 'comments',
+            localField: '_id',
+            foreignField: 'postID',
+            as: 'comments',
+          },
+        },
+        {
+          $lookup: {
+            from: 'likes',
+            localField: '_id',
+            foreignField: 'postID',
+            as: 'likes',
+          },
+        },
+        {
+          $lookup: {
+            from: 'circles',
+            localField: 'circleID',
+            foreignField: '_id',
+            as: 'circle',
+          },
+        },
+        {
+          $unwind: {
+            path: '$circle',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $project: {
+            _id: 1,
+            UID: 1,
+            caption: 1,
+            category: 1,
+            createdOn: 1,
+            circleID: 1,
+            tags: 1,
+            mediaURLs: 1,
+            circle: {
+              _id: 1,
+              circleName: 1,
+              UID: 1,
+            },
+            comments: {
+              _id: 1,
+              postID: 1,
+              UID: 1,
+              commentText: 1,
+            },
+            likes: {
+              _id: 1,
+              postID: 1,
+              UID: 1,
+              commentID: 1,
+            },
+          },
+        },
+      ])
+      .toArray();
     res.status(200).json({ 
       success: true, 
       message: "All Posts",
